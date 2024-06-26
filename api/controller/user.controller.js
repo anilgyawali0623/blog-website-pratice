fb4308a9cf0a46856c6d407da8ec2f0574ee1120
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-06-26 16:05:50 +0545

    this

---
+++ api/controller/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+  res.json({ message: " this is working in  correct way" });
+};
+
+export default test;
--- api/index.js
+++ api/index.js
@@ -1,11 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
+dotenv.config();
+
 const app = express();
- app.get("/", (res, req, next)=>{
-     res.send({
-          name:"anil"
-       })
- })
-app.listen(3000, (res, req) => {
+app.use(express.json());
+app.get("/", (req, res, next) => {
+  res.send({
+    name: "anil",
+  });
+});
+app.listen(process.env.PORT, (req, res) => {
   console.log("this server is working");
-   
+});
+
+app.use("/api/user", userRoutes);
+// incase the server doesnot connect with the server do this middleware
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
